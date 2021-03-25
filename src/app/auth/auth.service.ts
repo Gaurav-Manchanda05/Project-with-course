b52d2415ccad26bfa65c2f3cccf1542639f2dd4f
gauravmanchanda05 <>
2021-03-25 16:56:06 -0400

    Reflecting the Auth State in the UI

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y",
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDgn4o-Ue1Lli1wR0cLYTdevwZ4fDIy3U8",
         {
           email: email,
           password: password,
@@ -46,7 +46,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y",
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDgn4o-Ue1Lli1wR0cLYTdevwZ4fDIy3U8",
         {
           email: email,
           password: password,
