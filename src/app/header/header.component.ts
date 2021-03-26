e0be1f6d955e8c5d04bf52839f54ec1685aed92b
gauravmanchanda05 <>
2021-03-26 13:43:25 -0400

    Adding Logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -27,6 +27,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.dataStorageService.storeRecipes();
   }
 
+  onLogout() {
+    this.authService.logout();
+  }
+
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
   }
